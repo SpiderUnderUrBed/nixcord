f24483186c13b8d18cefb5957ab4e533685b646c
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-30 22:18:12 +1300

    fixed path support #13

--- hm-module.nix
+++ hm-module.nix
@@ -125,8 +125,8 @@ let
 
         installPhase = ''
           mkdir -p $out
-          echo "Copying contents to output"
-          cp -r . "$out"
+          echo "Copying contents from ${src} to output"
+          cp -r ${src}/* $out
         '';
       });
 
