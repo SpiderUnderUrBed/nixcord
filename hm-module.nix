fa2e20e4d9b6c478e718eae926e81471068fcc94
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-31 11:49:42 +1300

    changed rootPath to patchedVencord

--- hm-module.nix
+++ hm-module.nix
@@ -102,7 +102,7 @@ let
   else
     throw "Failed to extract a valid filepath from the given value";
 #in coerceGit
-  rootPath = builtins.toPath vencordPkgs;
+  rootPath = builtins.toPath patchedVencord;
 
   # Mapper function that applies coercion based on the regex match
   pluginMapper = plugin: 
