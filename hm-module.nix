1fd0501b0ecaada537c6eaab9934f51b824dec21
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-30 23:40:53 +1300

    fixed path support #38

--- hm-module.nix
+++ hm-module.nix
@@ -114,15 +114,15 @@ let
       plugin
     else
       lib.traceValFn (d: d.outPath) (pkgs.stdenv.mkDerivation {
-        name = "plugin-${builtins.hashString "sha256" (toString plugin)}";
-        src = builtins.toPath plugin; # Coerce `plugin` to a path
+      name = "plugin-${builtins.hashString "sha256" (toString plugin)}";
+      src = builtins.toPath plugin; # Coerce `plugin` to a path
+      
+      # Simplified unpack phase to copy files from src to the build directory
+      unpackPhase = ''
+        cp -r $src/. ./
+      '';
+    });
 
-        # Default unpack phase to copy files from src to the build directory
-        unpackPhase = ''
-          cp -r $src/* $TEMPDIR/$sourceRoot
-          & cd $TEMPDIR/$sourceRoot
-        '';
-      });
       #plugin;
       # builtins.path { 
       #   name = "plugin";
