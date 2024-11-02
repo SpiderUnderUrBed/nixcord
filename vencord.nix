bcf355b5acc4d78988626e963016e1a5c9f17c40
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-11-03 11:52:17 +1300

    141

--- vencord.nix
+++ vencord.nix
@@ -18,7 +18,7 @@ let
   pnpmToNPM = buildNpmPackage rec {
     pname = "pnpm-lock-to-npm-lock";
     version = "1.0.0";
-    #npmDepsHash = "sha256-dO1hAQduC7nyoVqWOVdc/OSfUf7atmA+zcuQhmmTmBM=";
+    npmDepsHash = "sha256-dO1hAQduC7nyoVqWOVdc/OSfUf7atmA+zcuQhmmTmBM=";
     src = fetchgit {
       url = "https://github.com/jakedoublev/pnpm-lock-to-npm-lock.git";
       hash = "sha256-dO1hAQduC7nyoVqWOVdc/OSfUf7atmA+zcuQhmmTmBM=";
