e941dbe5aadd48a56ecaf1fac97a82ece95ae84b
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-11-02 18:56:09 +1300

    multiple outputs 93

--- vencord.nix
+++ vencord.nix
@@ -1,7 +1,6 @@
 {
  # buildNpmPackage,
-  #fetchgit,
-  pkgs,
+  fetchgit,
   curl,
   esbuild,
   fetchFromGitHub,
@@ -15,30 +14,19 @@
   writeShellScript,
   buildWebExtension ? false,
 }:
-let
-  version = "1.10.5";
-  repo = fetchFromGitHub {
-    owner = "Vendicated";
-    repo = "Vencord";
-    rev = "v${version}";
-    hash = "sha256-pzb2x5tTDT6yUNURbAok5eQWZHaxP/RUo8T0JECKHJ4=";
-  };
-  nodeDeps = pkgs.runCommand "nodeDeps" {
-    buildInputs = [ pkgs.nodePackages.node2nix ];
-  } ''
-    mkdir -p $out
-    cp ${repo}/package.json $out
-    cd $out
-    node2nix -i package.json -o node-packages.nix
-  '';
-in
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "vencord";
-  version = version;
+  version = "1.10.5";
+  
+  outputs = ["out" "api" "node_modules"];
 
-  outputs = ["out" "api"];
-
-  src = repo;
+  src = lib.debug.traceValFn (v: "Fetched source path: ${v.outPath}") (fetchFromGitHub {
+    owner = "Vendicated";
+    repo = "Vencord";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-pzb2x5tTDT6yUNURbAok5eQWZHaxP/RUo8T0JECKHJ4=";
+  });
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname src;
@@ -72,19 +60,27 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildPhase = ''
     api_path=$api
+    node_module_path=$node_modules
 
     mkdir -p "$api_path"
     mv src/api/* "$api_path/"
     rmdir src/api
     ln -sf "$api_path" src/api
+
+    pnpm install .
     
+    mkdir -p "$node_module_path"
+    mv node_modules/* "$node_module_path/"
+    rmdir node_modules
+    ln -sf "$node_module_path" node_modules
+
     runHook preBuild
 
     substituteInPlace ./scripts/build/common.mjs \
       --replace-warn 'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*"]' \
-              'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*", "@api/*", "${(pkgs.callPackage "${nodeDeps}/default.nix" {}).nodeDependencies}/lib/node_modules/nanoid"]' \
+              'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*", "@api/*", "nanoid"]' \
       --replace-warn 'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin]' \
-        'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin, { name: "alias-plugin", setup(build) { build.onResolve({ filter: /^@api\// }, async (args) => { const fs = await import("fs"); const path = await import("path"); let resolvedPath = args.path.replace(/^@api/, "'"$api_path"'"); const extensions = [".ts", ".tsx", ".js", ".jsx"]; for (const ext of extensions) { const testPath = path.resolve(resolvedPath + ext); if (fs.existsSync(testPath)) { return { path: testPath }; } } if (fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isDirectory()) { resolvedPath = path.join(resolvedPath, "index"); for (const ext of extensions) { const testPath = resolvedPath + ext; if (fs.existsSync(testPath)) { return { path: testPath }; } } } return { path: resolvedPath }; }); } }]' \
+        'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin, { name: "api-alias-plugin", setup(build) { build.onResolve({ filter: /^@api\// }, async (args) => { const fs = await import("fs"); const path = await import("path"); let resolvedPath = args.path.replace(/^@api/, "'"$api_path"'"); const extensions = [".ts", ".tsx", ".js", ".jsx"]; for (const ext of extensions) { const testPath = path.resolve(resolvedPath + ext); if (fs.existsSync(testPath)) { return { path: testPath }; } } if (fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isDirectory()) { resolvedPath = path.join(resolvedPath, "index"); for (const ext of extensions) { const testPath = resolvedPath + ext; if (fs.existsSync(testPath)) { return { path: testPath }; } } } return { path: resolvedPath }; }); } }, { name: "nanoid-alias-plugin", setup(build) { build.onResolve({ filter: /^nanoid$/ }, (args) => { return { path: "$node_module_path/nanoid" }; }); } }]'  \
     #  --replace-warn 'esbuild.build({' \
     #         'esbuild.build({ resolveExtensions: [".ts", ".tsx", ".js", ".jsx"],'
 
