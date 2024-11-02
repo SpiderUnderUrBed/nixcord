1637bb8355798936cc7e39e44139b3968f2cdf48
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-11-02 15:58:14 +1300

    multiple outputs 68

--- vencord.nix
+++ vencord.nix
@@ -71,7 +71,9 @@ stdenv.mkDerivation (finalAttrs: {
       --replace 'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*"]' \
               'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*", "@api/*", "nanoid"]' \
       --replace 'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin]' \
-              'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin, { name: "alias-plugin", setup(build) { build.onResolve({ filter: /^@api\// }, function(args) { return { path: args.path.replace(/^@api/, "'"$api_path"'")}; }); } }, resolveExtensions: [".ts", ".tsx", ".js", ".jsx"]]'
+              'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin, { name: "alias-plugin", setup(build) { build.onResolve({ filter: /^@api\// }, function(args) { return { path: args.path.replace(/^@api/, "'"$api_path"'") }; }); } }]' \
+      --replace 'esbuild.build({' \
+              'esbuild.build({ resolveExtensions: [".ts", ".tsx", ".js", ".jsx"],'
 
     runHook preBuild
 
