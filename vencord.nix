f48f91a54b3a3ae74e29d83ae029bf31fa882e59
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-11-03 11:44:09 +1300

    138

--- vencord.nix
+++ vencord.nix
@@ -22,8 +22,8 @@ let
     src = fetchgit {
       url = "https://github.com/jakedoublev/pnpm-lock-to-npm-lock.git";
       #hash = "sha256-dO1hAQduC7nyoVqWOVdc/OSfUf7atmA+zcuQhmmTmBM=";
-      ref = "main";
-      rev = "a67f35286dfd6feba64a010e1b1005b6aa220e86";  
+      ref = "a67f35286dfd6feba64a010e1b1005b6aa220e86";
+      #rev = "a67f35286dfd6feba64a010e1b1005b6aa220e86";  
     };
   };
   npmDeps = buildNpmPackage rec {
