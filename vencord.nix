b2bed5b62d84d2d66ffb3a3750d7309b4db89aad
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-11-02 17:48:20 +1300

    multiple outputs 82

--- vencord.nix
+++ vencord.nix
@@ -68,7 +68,7 @@ stdenv.mkDerivation (finalAttrs: {
 
     substituteInPlace ./scripts/build/common.mjs \
       --replace-warn 'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*"]' \
-              'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*", "@api/*", "${pkgs.nodePackages.nanoidPath}"]' \
+              'external: ["~plugins", "~git-hash", "~git-remote", "/assets/*", "@api/*", "${pkgs.nodePackages.nanoid}"]' \
       --replace-warn 'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin]' \
         'plugins: [fileUrlPlugin, gitHashPlugin, gitRemotePlugin, stylePlugin, { name: "alias-plugin", setup(build) { build.onResolve({ filter: /^@api\// }, async (args) => { const fs = await import("fs"); const path = await import("path"); let resolvedPath = args.path.replace(/^@api/, "'"$api_path"'"); const extensions = [".ts", ".tsx", ".js", ".jsx"]; for (const ext of extensions) { const testPath = path.resolve(resolvedPath + ext); if (fs.existsSync(testPath)) { return { path: testPath }; } } if (fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isDirectory()) { resolvedPath = path.join(resolvedPath, "index"); for (const ext of extensions) { const testPath = resolvedPath + ext; if (fs.existsSync(testPath)) { return { path: testPath }; } } } return { path: resolvedPath }; }); } }]' \
     #  --replace-warn 'esbuild.build({' \
